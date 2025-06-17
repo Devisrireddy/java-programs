f83ad70455d391fec268251324ded4ae353e3a6d
Devisrireddy <devisrireddy48@gmail.com>
2025-06-17 13:58:07 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		System.out.println(n);
+	}
+
+}
